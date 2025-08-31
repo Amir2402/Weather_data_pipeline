fdc44860e32a02c2ca92d94b69cecc69c81e0b02
Amir2402 <benameur.amir7@gmail.com>
2025-08-31 18:16:31 +0100

    fixed requests issue

--- makefile
+++ makefile
@@ -2,6 +2,5 @@ start:
 	sudo astro dev start --image-name yahoo-pipeline_8f88da/airflow
 stop: 
 	sudo astro dev stop
-
 restart: 
 	sudo astro dev restart --image-name yahoo-pipeline_8f88da/airflow 
--- plugins/operators/loadDailyWeatherData.py
+++ plugins/operators/loadDailyWeatherData.py
@@ -4,6 +4,8 @@ from botocore.client import Config
 import requests as rq 
 import json 
 from datetime import datetime 
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 
 class LoadDailyWeatherData(BaseOperator):
     def __init__(self, access_key, secret_key, endpoint_url, **kwargs):
@@ -19,6 +21,7 @@ class LoadDailyWeatherData(BaseOperator):
         aws_access_key_id=self.access_key,
         aws_secret_access_key=self.secret_key
         )
+        self.log.info('task started!')
         URL = "https://api.open-meteo.com/v1/forecast"
 
         lat_long_list = self.get_latitude_longitude(s3)
@@ -27,7 +30,6 @@ class LoadDailyWeatherData(BaseOperator):
 
 
     def get_latitude_longitude(self, s3): 
-        print('test')
         lat_long_object = s3.Object('bronze', 'latitude_longitude.csv')
         file_content = lat_long_object.get()['Body'].read().decode('utf-8')
         lat_long_list = []
@@ -36,20 +38,23 @@ class LoadDailyWeatherData(BaseOperator):
             line = line.split(',')
             lat_long_list.append((line[1], line[2]))
 
-        return lat_long_list
+        return lat_long_list[:200]
     
     def get_data_from_weather_api(self, lat_long_list, url): 
         requests_results = []
+        session = self.requests_session_with_retries()
 
         for lat, long in lat_long_list: 
-            res = rq.get(url, params = {
+            res = session.get(url, params = {
                 "latitude": lat,
                 "longitude": long,
                 "hourly": "temperature_2m",
                 "forecast_days": 1,
             })
-            print(res)
-            requests_results.append(res.json())
+            
+            if res.status_code == 200: 
+                self.log.info(res)
+                requests_results.append(json.loads(res.content))
         
         return requests_results  
     
@@ -58,13 +63,26 @@ class LoadDailyWeatherData(BaseOperator):
         today_month = datetime.now().month
         today_year = datetime.now().year
 
-        s3object = s3.Object('bronze', f'daily_temp_data{today_month}_{today_day}_{today_year}.json')
+        s3object = s3.Object('bronze', f'daily_temp_data_{today_month}_{today_day}_{today_year}.json')
         
         daily_temp_object = {
             'result': requests_results
         }
-
         s3object.put(
             Body=(bytes(json.dumps(daily_temp_object).encode('UTF-8'))),
             ContentType="application/json"
         )
+        self.log.info("Weather data loaded succefully!")
+
+
+    def requests_session_with_retries(self):
+        session = rq.Session()
+        retries = Retry(
+            total=5,    
+            backoff_factor=1,
+            status_forcelist=[429, 500, 502, 503, 504],
+        )
+        adapter = HTTPAdapter(max_retries=retries)
+        session.mount("http://", adapter)
+        session.mount("https://", adapter)
+        return session
